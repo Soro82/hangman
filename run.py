3ac1751b379d4280a02b18c7562ee348620fcf6d
Gary Sorohan <148573610+Soro82@users.noreply.github.com>
2024-02-17 15:03:35 +0000

    add menu options and ask user for their choice

--- run.py
+++ run.py
@@ -1,1 +1,16 @@
-print("Welcome to Hangman")
+def run_game():
+    print("Welcome to Hangman\n")
+    print("Menu Options:")
+    print("1. Play Game")
+    print("2. Rules")
+    print("3. Exit Game\n")
+    print("Please choose a Menu Option")
+
+    menu_option = input("Enter 1, 2 or 3\n")
+    print(menu_option)
+          
+
+
+
+
+run_game()
