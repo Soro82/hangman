29c7c437b692be3a8e32e5839f6f9c9b88054b98
Gary Sorohan <148573610+Soro82@users.noreply.github.com>
2024-02-22 16:29:48 +0000

    remove validate input function

--- run.py
+++ run.py
@@ -65,29 +65,36 @@ def run_game():
     print("3. Exit Game\n")
     print("Please choose a Menu Option.")
 
-    menu_option = input("Enter 1, 2 or 3\n")
-    validate_input(menu_option)
+    while True:
+        menu_option = input("Enter 1, 2 or 3\n")
+        if menu_option not in ("1","2","3"):
+            print("Invalid data entered. Please choose a Menu Option.\n.")
+        else:
+            break
+
+    if menu_option == "1":
+        play_game()
+    elif menu_option == "2":
+        display_rules()
+    elif menu_option == "3":
+        quit()
 
 
-def validate_input(choice):
+def display_rules():
     """
-    Check the user's input to ensure they enter a correct Menu option.
+    Prints the rules of the game.
     """
-    try:
-        int_choice = int(choice)
-        if int_choice == 1:
-            play_game()
-        # elif menu_option == 2:
-        #     display_rules()
-        elif int_choice == 3:
-            quit()
-        # else:
-         #     print("Invalid data entered. Please choose a Menu Option.")
-         #     run_game()
-        
-    except ValueError:
-        print("Invalid data entered. Please choose a Menu Option.\n")
-                   
+    print('''
+    The Rules of the game are:\n
+    1. A word will be picked at random.
+    2. An underscore will be displayed for each letter in the word.
+    3. You will have 6 chances to guess every letter in the word.
+    4. When you get a letter correct it will replace the underscore.
+    5. When you get a letter incorrect a line will be added to the man.
+    6. The game is over when you get all the letters correct or
+       the image of the man is complete.
+          ''')
+    play_game()
 
 def pick_random_word():
     """
@@ -110,7 +117,7 @@ def print_blank_word(word_to_print):
         blank_word = blank_word+"_ "
 
     print(blank_word)
-    print(word_to_print)
+    print(word_to_print) # Delete this line
 
 
 def ask_for_answer(word):
